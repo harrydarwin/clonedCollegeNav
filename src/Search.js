ea3f51e48b5b77d9adfd41a3d23d458f25bffafa
Harry <harryguloien@gmail.com>
2020-12-07 15:14:42 -0500

    trying to pull master

--- src/Search.js
+++ src/Search.js
@@ -3,18 +3,25 @@ import axios from 'axios'
 
 
 //Grab array of things and filter again for category name including university, college etc to get only schools/post secondary institutes
+
+//QUERIES FOR SCHOOLS----
+//results.data.response.venues.categories.name:
+// "General College & University"
+// "University"
+// "College Academic Building"
+
 class Search extends Component {
     componentDidMount(){
         console.log('ITS ATTACHED');
         axios({
             method: 'GET',
             responseType: 'json',
-            url: 'https://api.foursquare.com/v2/venues/search',
+            url: 'https://api.foursquare.com/v2/venues/categories',
             params: {
                 client_id:'SMUUEFGVRENHIW3EQX5ICCFCTNQPPIWVXP21E2BQVRH421OF',
                 client_secret:'EVNPHQ3EYKNQKZMOAKRVUTT0KDHXXGNUWUCY0LFZTVRE2BAF',
                 near: 'Toronto, ON',
-                query: "General College & University",
+                query: "trade school",
                 v: 20201205
             }
         }).then((res) => {
